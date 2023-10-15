3cb89644331221f187507c9b445dfa55959ab4ab
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2023-10-15 12:25:34 +0200

    Z_26ASSETS → Z_28ASSETS; Z_26ALBUMS → Z_28ALBUMS;

--- select.sql
+++ select.sql
@@ -1,5 +1,6 @@
 -- select ZDIRECTORY,ZFILENAME,* from ZASSET limit 100;
-select ZTITLE,(ZDIRECTORY||'/'||ZFILENAME) as ZPATH from ZGENERICALBUM a
-inner join Z_26ASSETS j on a.Z_PK = j.Z_26ALBUMS
-inner join ZASSET f on f.Z_PK = j.Z_3ASSETS
-order by ZTITLE desc;
+
+select ZTITLE,(ZDIRECTORY||'/'||ZFILENAME) as ZPATH from ZGENERICALBUM albums
+inner join Z_28ASSETS albumassets on albums.Z_PK = albumassets.Z_28ALBUMS
+inner join ZASSET assets on assets.Z_PK = albumassets.Z_3ASSETS
+order by ZTITLE desc
